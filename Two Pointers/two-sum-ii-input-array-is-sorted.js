5078e47e972b9e723c1267bb2a2492bb09e4e964
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2026-03-21 18:41:07 -0600

    two sum ii

--- Two Pointers/two-sum-ii-input-array-is-sorted.js
+++ Two Pointers/two-sum-ii-input-array-is-sorted.js
@@ -1,1 +1,31 @@
-var twoSum = function (numbers, target) {};
+//** https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+/*
+Problem: Two Sum II - Input Array Is Sorted
+Category: Two Pointers
+Pattern: Opposite Direction Two Pointers
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Key Idea:
+Because the array is sorted, we can compare the sum of the smallest
+and largest elements and move pointers inward to shrink the search space.
+*/
+
+var twoSum = function (numbers, target) {
+  let l = 0; // left pointer (smallest value)
+  let r = numbers.length - 1; // right pointer (largest value)
+
+  while (l < r) {
+    const sum = numbers[l] + numbers[r]; // current pair sum
+
+    if (sum > target) {
+      r--; // sum too big → move right pointer left
+    } else if (sum < target) {
+      l++; // sum too small → move left pointer right
+    } else {
+      return [l + 1, r + 1]; // found pair (problem requires 1-based index)
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
